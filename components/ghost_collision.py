6ce1facbd4ce05899598c8bc6b550063081dae87
yaroslav <maplegend@mail.ru>
2019-12-16 18:12:11 +0500

    Added returning to base

--- components/ghost_collision.py
+++ components/ghost_collision.py
@@ -2,6 +2,10 @@ from pysmile.component import Component
 from pysmile.components.transform import TransformComponent
 from pysmile.events.update import UpdateEvent
 from pysmile.components.collisions.collider import Collider
+
+from components.base_run import BaseRunComponent
+from components.scary_mode import ScaryModeComponent
+from events.add_score import AddScoreEvent
 from events.game_over import GameOverEvent
 
 
@@ -20,6 +24,12 @@ class GhostCollision(Component):
         for ghost in self.ghosts:
             ghost_col = ghost.get_component(Collider).get_collider()[0]
             if collider.colliderect(ghost_col):
+                if ghost.contains_component(BaseRunComponent):
+                    return
+                if ghost.get_component(ScaryModeComponent).scary:
+                    self.entity.event_manager.trigger_event(AddScoreEvent(200))
+                    ghost.add_component(BaseRunComponent())
+                    return
                 self.entity.event_manager.trigger_event(GameOverEvent())
                 self.entity.remove_component(GhostCollision)
                 return
